5e6123137391a85ab693a326b099f4efaa30b2fb
0701MF20 <farazmohd0701@gmail.com>
2021-07-10 17:25:45 +0530

    up button(Basic functionality)+three dot menu

--- app/src/main/java/com/example/smallactivity/MainActivity.java
+++ app/src/main/java/com/example/smallactivity/MainActivity.java
@@ -27,5 +27,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflate=getMenuInflater();
+        inflate.inflate(R.menu.example1_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1id:
+             Toast.makeText(this,"Item 1",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2id:
+                Toast.makeText(this,"Item 2",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3id:
+                 Toast.makeText(this,"Item 3",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4id:
+                Toast.makeText(this,"Item 4",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item5id:
+                Toast.makeText(this,"Item 5",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item6id:
+                Toast.makeText(this,"Item 6",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
